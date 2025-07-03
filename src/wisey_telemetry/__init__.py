309c92671374a641b53bb1d259a407a6369049ba
xxykzdev <xxykzdev@gmail.com>
2025-07-03 01:18:54 -0300

    fix

--- src/wisey_telemetry/__init__.py
+++ src/wisey_telemetry/__init__.py
@@ -1,3 +1,17 @@
-from .telemetry import init_telemetry, instrument_app, get_tracer, start_trace_span
+# wisey_telemetry/__init__.py
 
-__all__ = ["init_telemetry", "instrument_app", "get_tracer", "start_trace_span"]
+from .telemetry import (
+    init_telemetry,
+    instrument_app,
+    get_tracer,
+    start_trace_span,
+    trace_function,
+)
+
+__all__ = [
+    "init_telemetry",
+    "instrument_app",
+    "get_tracer",
+    "start_trace_span",
+    "trace_function"
+]
